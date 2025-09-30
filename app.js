e700b17267b29346dc0a9e5f41ed67b3afac00b9
Shema <shemaphilbert8@gmail.com>
2025-09-30 12:00:00 +0200

    chore: commit for Sep 30, 2025

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+
+//register view engine
+app.set('view engine','ejs')
+
+// listen to requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express</h1>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 })
 app.get('/about', (req, res) => {
     // res.send('<h1>Hello express</h1>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 })
 
 app.get('/about-us', (req, res) => {
@@ -17,5 +22,5 @@ app.get('/about-us', (req, res) => {
 })
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 })
