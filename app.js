51090b72d7164c3181ae4b46b2fb900b4914d04e
Shema <shemaphilbert8@gmail.com>
2025-09-30 10:18:49 +0200

    using express for routing

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello express</h1>');
+    // res.send('<h1>Hello express</h1>');
+    res.sendFile('./views/index.html', {root: __dirname});
+})
+app.get('/about', (req, res) => {
+    // res.send('<h1>Hello express</h1>');
+    res.sendFile('./views/about.html', {root: __dirname});
+})
+
+app.get('/about-us', (req, res) => {
+    // res.send('<h1>Hello express</h1>');
+    res.redirect('/about');
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
 })
